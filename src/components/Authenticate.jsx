e76639e7f6b210d41a4edf9fe1418f3f1384c411
James_Pope <jamesxpope@gmail.com>
2024-06-18 13:37:18 -0400

    ReactForms

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Authenticate({ token }) {
   const [successMessage, setSuccessMessage] = useState(null);
